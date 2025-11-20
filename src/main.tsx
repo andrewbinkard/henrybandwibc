f136b249356e464a864447d8898c0e58960b605f
andrewbinkard <andrewbinkard@gmail.com>
2025-11-19 21:25:26 -0600

    add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import App from "./App.tsx";
 import "./main.scss";
 import { BrowserRouter } from "react-router-dom";
 import { routes } from "./Routes/routes.tsx";
+import { Analytics } from "@vercel/analytics/next";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <App routes={routes} />
+      <Analytics />
     </BrowserRouter>
   </StrictMode>
 );
